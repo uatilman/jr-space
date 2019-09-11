7dbdf18e4deffd365093d2e9fea1874acb273395
uatilman <uatilman@gmail.com>
2019-09-11 14:44:15 +0300

    Add custom Json bean

--- src/main/java/com/space/config/WebConfig.java
+++ src/main/java/com/space/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.space.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -21,7 +23,6 @@ import java.util.List;
 @EnableWebMvc
 @ComponentScan("com.space.controller")
 public class WebConfig implements WebMvcConfigurer {
-
 
     @Bean
     public ViewResolver internalResourceViewResolver() {
@@ -42,11 +43,18 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
+    @Bean
+    public Jackson2ObjectMapperBuilder customJson() {
+        return new Jackson2ObjectMapperBuilder()
+                .indentOutput(true);
+    }
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.getObjectMapper().setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         converter.getObjectMapper().setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        converter.getObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
         converters.add(converter);
     }
 }
